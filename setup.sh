edfe61d8077c3a00443c387f17b4872b0ca0bea6
Phill Conrad <pconrad@cs.ucsb.edu>
2017-09-25 13:31:36 -0700

    update Gemfile to ruby 2.4.0

--- setup.sh
+++ setup.sh
@@ -2,8 +2,9 @@
 
 echo "Installing software needed to run Jekyll locally... "
 [[ -s "$HOME/.rvm/scripts/rvm" ]] && source "$HOME/.rvm/scripts/rvm" 
-rvm install ruby-2.1.7
-rvm use 2.1.7
+rvm install ruby-2.4.0
+rvm use 2.4.0
 gem install bundler
+gem install jekyll
 bundle install --path vendor/bundle
 echo "Done."
